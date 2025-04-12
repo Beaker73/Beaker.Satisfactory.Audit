13ba62e1f2fd690d277a11ed68c53dfe0132bcaf
Jeroen Smits <beaker73@live.nl>
2025-04-12 12:04:28 +0200

    Fix copilation issues

--- src/Components/IngredientTile/Story.tsx
+++ src/Components/IngredientTile/Story.tsx
@@ -1,5 +1,6 @@
 import { Card, makeStyles } from "@fluentui/react-components";
 import { IngredientTile } from ".";
+import type { ItemKey } from "../../Database/Types";
 import type { StoryExport } from "../StoryPage/Types";
 
 function IngredientTileStory()
@@ -12,33 +13,33 @@ function IngredientTileStory()
 			<h4>Size</h4>
 			<p>An Ingredient Tile supports <em>tiny</em>, <em>small</em>, <em>medium</em>, <em>large</em> and <em>huge</em>. If not given the tile defaults to the large size.</p>
 			<Card className={style.examples}>
-				<IngredientTile itemKey="Desc_IronPlate_C" size="tiny" />
-				<IngredientTile itemKey="Desc_IronPlate_C" size="small" />
-				<IngredientTile itemKey="Desc_IronPlate_C" size="medium" />
-				<IngredientTile itemKey="Desc_IronPlate_C" size="large" />
-				<IngredientTile itemKey="Desc_IronPlate_C" size="huge" />
+				<IngredientTile itemKey={"Desc_IronPlate_C" as ItemKey} size="tiny" />
+				<IngredientTile itemKey={"Desc_IronPlate_C" as ItemKey} size="small" />
+				<IngredientTile itemKey={"Desc_IronPlate_C" as ItemKey} size="medium" />
+				<IngredientTile itemKey={"Desc_IronPlate_C" as ItemKey} size="large" />
+				<IngredientTile itemKey={"Desc_IronPlate_C" as ItemKey} size="huge" />
 			</Card>
 		</section>
 		<section>
 			<h4>Quantity</h4>
 			<p>For use in recipies there is an option to show quantities of the item. The size needs to be at least medium to be able to show a quantity. Smaller sizes are to small for this. The tooltip however does show the quantity in such cases.</p>
 			<Card className={style.examples}>
-				<IngredientTile itemKey="Desc_IronPlate_C" size="tiny" quantity={31} />
-				<IngredientTile itemKey="Desc_IronPlate_C" size="small" quantity={6} />
-				<IngredientTile itemKey="Desc_IronPlate_C" size="medium" quantity={91} />
-				<IngredientTile itemKey="Desc_IronPlate_C" size="large" quantity={133} />
-				<IngredientTile itemKey="Desc_IronPlate_C" size="huge" quantity={285} />
+				<IngredientTile itemKey={"Desc_IronPlate_C" as ItemKey} size="tiny" quantity={31} />
+				<IngredientTile itemKey={"Desc_IronPlate_C" as ItemKey} size="small" quantity={6} />
+				<IngredientTile itemKey={"Desc_IronPlate_C" as ItemKey} size="medium" quantity={91} />
+				<IngredientTile itemKey={"Desc_IronPlate_C" as ItemKey} size="large" quantity={133} />
+				<IngredientTile itemKey={"Desc_IronPlate_C" as ItemKey} size="huge" quantity={285} />
 			</Card>
 		</section>
 		<section>
 			<h4>Item</h4>
 			<p>By providing the item key the correct item is shown in the tile</p>
 			<Card className={style.examples}>
-				<IngredientTile itemKey="Desc_IronPlate_C" />
-				<IngredientTile itemKey="Desc_NobeliskNuke_C" />
-				<IngredientTile itemKey="Desc_Crystal_C" />
-				<IngredientTile itemKey="Desc_Motor_C" />
-				<IngredientTile itemKey="Desc_FicsiteIngot_C" />
+				<IngredientTile itemKey={"Desc_IronPlate_C" as ItemKey} />
+				<IngredientTile itemKey={"Desc_NobeliskNuke_C" as ItemKey} />
+				<IngredientTile itemKey={"Desc_Crystal_C" as ItemKey} />
+				<IngredientTile itemKey={"Desc_Motor_C" as ItemKey} />
+				<IngredientTile itemKey={"Desc_FicsiteIngot_C" as ItemKey} />
 			</Card>
 		</section>
 	</article>
--- src/Components/RecipeFlow/Story.tsx
+++ src/Components/RecipeFlow/Story.tsx
@@ -1,5 +1,6 @@
 import { Card, makeStyles, tokens } from "@fluentui/react-components";
 import { RecipeFlow } from ".";
+import type { RecipeKey } from "../../Database/Types";
 import type { StoryExport } from "../StoryPage/Types";
 
 function RecipeFlowStory()
@@ -12,20 +13,20 @@ function RecipeFlowStory()
 			<h4>Default</h4>
 			<p>Simply provided the key to the recipy and the flow of the correct recipe will be displayed</p>
 			<Card className={style.examples}>
-				<RecipeFlow recipeKey="Recipe_IngotIron_C" />
-				<RecipeFlow recipeKey="Recipe_AluminaSolution_C" />
-				<RecipeFlow recipeKey="Recipe_PlutoniumFuelRod_C" />
+				<RecipeFlow recipeKey={"Recipe_IngotIron_C" as RecipeKey} />
+				<RecipeFlow recipeKey={"Recipe_AluminaSolution_C" as RecipeKey} />
+				<RecipeFlow recipeKey={"Recipe_PlutoniumFuelRod_C" as RecipeKey} />
 			</Card>
 		</section>
 		<section>
 			<h4>Sizes</h4>
 			<p>The flow comes in the same 5 sizes as the individual tiles, <em>tiny</em>, <em>small</em>, <em>medium</em>, <em>large</em> and <em>huge</em>. If not given the flow defaults to the large size.</p>
 			<Card className={style.examples}>
-				<RecipeFlow recipeKey="Recipe_AluminaSolution_C" size="tiny" />
-				<RecipeFlow recipeKey="Recipe_AluminaSolution_C" size="small" />
-				<RecipeFlow recipeKey="Recipe_AluminaSolution_C" size="medium" />
-				<RecipeFlow recipeKey="Recipe_AluminaSolution_C" size="large" />
-				<RecipeFlow recipeKey="Recipe_AluminaSolution_C" size="huge" />
+				<RecipeFlow recipeKey={"Recipe_AluminaSolution_C" as RecipeKey} size="tiny" />
+				<RecipeFlow recipeKey={"Recipe_AluminaSolution_C" as RecipeKey} size="small" />
+				<RecipeFlow recipeKey={"Recipe_AluminaSolution_C" as RecipeKey} size="medium" />
+				<RecipeFlow recipeKey={"Recipe_AluminaSolution_C" as RecipeKey} size="large" />
+				<RecipeFlow recipeKey={"Recipe_AluminaSolution_C" as RecipeKey} size="huge" />
 			</Card>
 		</section>
 	</article>
